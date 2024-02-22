4500b59facbde63e9b73535f3a55f68f1ac92f71
Ardentem <ardentemwang@gmail.com>
2024-02-22 20:20:25 +0800

    Site updated: 2024-02-22 20:20:25

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -106,7 +106,7 @@ function loadWidget(config) {
 //			Live2D.captureFrame = true;
 //		});
 		document.querySelector("#waifu-tool .fa-info-circle").addEventListener("click", () => {
-			open("https://github.com/stevenjoezhang/live2d-widget");
+			open("https://ochio.fun/2024/02/22/live2dmoc3/");
 		});
 		document.querySelector("#waifu-tool .fa-times").addEventListener("click", () => {
 			localStorage.setItem("waifu-display", Date.now());
